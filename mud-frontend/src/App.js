438d1bddf76d856f826b35b6e67476edf067a930
Abigayle Franklin <abifanklin@gmail.com>
2019-07-11 23:17:39 -0500

    Initial frontend layout

--- mud-frontend/src/App.js
+++ mud-frontend/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "primereact/resources/themes/nova-dark/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import Header from './Components/Header';
+import RoomInventory from './Components/RoomInventory'
+import GraphPlaceholder from './Components/GraphPlaceholder'
+import PersonalInventory from './Components/Inventory'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <RoomInventory />
+      <div className="lower">
+        <GraphPlaceholder />
+        <PersonalInventory />
+      </div>
     </div>
   );
 }
---
+++ mud-frontend/src/Components/Inventory.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Card} from 'primereact/card';
+
+const PersonalInventory = () => {
+    return (
+        <div>
+            <Card header="Personal Inventory" className="inventory">
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                </ul>
+            </Card>
+        </div>
+    )
+}
+
+export default PersonalInventory;
