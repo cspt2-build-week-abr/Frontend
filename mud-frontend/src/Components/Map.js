b2c8f821347dcb2675181603594148d693eef4da
bcabanayan <bcabanayan@gmail.com>
2019-07-14 19:01:05 -0700

    Created mock coordinates and experimented with line mark series

--- mud-frontend/src/Components/Map.js
+++ mud-frontend/src/Components/Map.js
@@ -1,26 +1,28 @@
 import React  from 'react';
 import '../App.css';
 import '../../node_modules/react-vis/dist/style.css';
-import {XYPlot, MarkSeries} from 'react-vis';
+import {XYPlot, MarkSeries, LineMarkSeries} from 'react-vis';
 
 class Map extends React.Component {
   render() {
     const data = [
-      {x: 0, y: 8},
-      {x: 1, y: 5},
-      {x: 2, y: 4},
-      {x: 3, y: 9},
-      {x: 4, y: 1},
-      {x: 5, y: 7},
-      {x: 6, y: 6},
-      {x: 7, y: 3},
-      {x: 8, y: 2},
-      {x: 9, y: 0}
+        {x: 0, y: 0},
+        {x: 0, y: 1},
+        {x: 0, y: 2},
+        {x: 1, y: 1},
+        {x: 1, y: 2},
+        {x: 1, y: 3},
+        {x: 2, y: 2},
+        {x: 3, y: 3}
     ];
     return (
       <div>
         <XYPlot height={300} width={300}>
-          <MarkSeries data={data} />
+        <LineMarkSeries
+            data={data}
+            lineStyle={{stroke:"red"}}
+            markStyle={{stroke:"blue"}}
+        />
         </XYPlot>
       </div>
     );
