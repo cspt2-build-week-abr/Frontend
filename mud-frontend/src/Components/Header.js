8fe5ebc6079c5f81b2c845754e25770a979f3aa5
Abigayle Franklin <abifranklin@gmail.com>
2019-07-17 08:49:33 -0500

    Some styling changes to map and added components for sign up and log in

--- mud-frontend/src/Components/Header.js
+++ mud-frontend/src/Components/Header.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {Button} from 'primereact/button'
+import {OverlayPanel} from 'primereact/overlaypanel';
+import {InputText} from 'primereact/inputtext';
 
 class Header extends Component{
     render(){
@@ -7,7 +9,24 @@ class Header extends Component{
         <div className="header">
             <h1>OKEMONPAY!</h1>
             <h2>Current Room: {this.props.room}</h2>
-            <Button label="Log In" className="p-button-raised p-button-rounded p-button-secondary"/>
+            <div className='headbuttongroup'>
+                <Button label="Sign Up" className="p-button-raised p-button-rounded p-button-secondary headbutton" onClick={(e) => this.op1.toggle(e)}/>
+                <Button label="Log In" className="p-button-raised p-button-rounded p-button-secondary headbutton" onClick={(e) => this.op2.toggle(e)}/>
+            </div>
+            <OverlayPanel ref={(el) => this.op1 = el}>
+                <div className='auth'>
+                 <InputText placeholder="Username" />
+                 <InputText placeholder="Password" />
+                 <Button label="Submit" className="p-button-rounded p-button-success" />
+                </div>
+            </OverlayPanel>
+            <OverlayPanel ref={(el) => this.op2 = el}>
+            <div className='auth'>
+                 <InputText placeholder="Username" />
+                 <InputText placeholder="Password" />
+                 <Button label="Submit" className="p-button-rounded p-button-success" />
+                </div>
+            </OverlayPanel>
         </div>
     )}
 }
