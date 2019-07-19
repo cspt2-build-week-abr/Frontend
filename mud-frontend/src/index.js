1edb54a88c3c1b1dfd1aab5f708e221033dbb5e8
Ryan Matthews <ryan.matthews.professional@gmail.com>
2019-07-18 20:48:10 -0500

    adjust implementation of apollo

--- mud-frontend/src/index.js
+++ mud-frontend/src/index.js
@@ -4,18 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ApolloClient, { gql } from "apollo-boost";
-import { ApolloProvider } from 'react-apollo'
 
 const client = new ApolloClient({
     uri: "https://mud-pokemon.herokuapp.com/graphql"
 })
 
 
-ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>
-    , document.getElementById('root'));
+ReactDOM.render(<App client={client}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
